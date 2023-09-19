10a6f1054eb16d3748ac4fb3d997f8e2896d2a86
TDMWalpala <tharindudarshana2019@gmail.com>
2023-09-19 17:58:59 +0530

    Hand tracking module compleated

---
+++ MyHandTrackingGM.py
@@ -0,0 +1,20 @@
+
+import cv2
+import mediapipe as mp
+import time
+import Hand_Tracking_Module as htm
+pTime = 0
+cTime = 0
+cap = cv2.VideoCapture(1)
+detector = htm.handDetector()
+while True:
+    success, img = cap.read()
+    img = detector.findHands(img, draw=True )
+    lmList = detector.findPosition(img, draw=False)
+    if len(lmList) != 0:
+        print(lmList[4])
+    cTime = time.time()
+    fps = 1 / (cTime - pTime)
+    pTime = cTime
+    cv2.imshow("Image", img)
+    cv2.waitKey(1)
